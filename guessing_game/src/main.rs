0bfcc214c14db8c15885b05a12397d3858e017ef
Bradley Neumaier <neumaierbradley@icloud.com>
2022-08-07 23:19:00 -0400

    p

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,9 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("Please input your guess");
     
     let mut guess = String::new();
